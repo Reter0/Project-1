591ea2b672b8f8cb602f4b3fa49fc904dac634c2
Rey <rey.f.0024@gmail.com>
2025-12-17 19:14:09 +0330

    FCFS COMPLETE

--- cpu/algorithms/fcfs.py
+++ cpu/algorithms/fcfs.py
@@ -2,7 +2,7 @@
 
 def fcfs(processes):
     """
-    TODO: Implement First-Come, First-Served (FCFS) CPU scheduling algorithm
+    Implement First-Come, First-Served (FCFS) CPU scheduling algorithm
     
     FCFS is a non-preemptive scheduling algorithm where processes are executed 
     in the order of their arrival time.
@@ -19,20 +19,29 @@ def fcfs(processes):
         - completion_times: Dictionary {pid: completion_time}
     """
     
-    # TODO: 
-    # Currently returning empty data - replace with your implementation
+    # TODO 1
+    processes = sorted(processes, key=lambda p: p[1])
     
+    # TODO 2
+    current_time = 0
     gantt_chart = []
     waiting_times = {}
     turnaround_times = {}
     completion_times = {}
     
-    # TODO: Step 1 - Sort processes by arrival time
-    
-    # TODO: Step 2 - Initialize current_time
-    
-    # TODO: Step 3 - Process each job in FCFS order
-    
-    # TODO: Step 4 - Return the results
-    
+    # TODO 3
+    for pid, arrival_time, burst_time in processes:
+        if current_time < arrival_time:
+            current_time = arrival_time
+        
+        start_time = current_time
+        end_time = current_time + burst_time
+        gantt_chart.append((pid, start_time, end_time))
+        
+        completion_times[pid] = end_time
+        turnaround_times[pid] = end_time - arrival_time
+        waiting_times[pid] = turnaround_times[pid] - burst_time
+        
+        current_time = end_time
+    # TODO 4
     return gantt_chart, waiting_times, turnaround_times, completion_times
